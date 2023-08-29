2fabcd28f93c3f46b453b419d409a27b9f7e50f9
Mustafa <mostafakhaled0787314@gmail.com>
2023-08-29 11:13:53 +0300

    new arrival section done

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -3,29 +3,59 @@ import photoOne from "../assets/arr1.png";
 import photoTwo from "../assets/arr2.png";
 import photoThree from "../assets/arr3.png";
 import photoFour from "../assets/arr4.png";
+import { Link } from "react-router-dom";
 
 function Featured() {
   return (
     <div>
       <SectionHead head="New Arrival" description="Featured" />
-      <div className=" flex flex-col items-start gap-[20px] md:flex-row">
-        <div className="w-full md:w-[50%]">
-          <img src={photoOne} alt="" className="w-full" />
-        </div>
 
-        <div className="w-full md:w-[50%]">
-          <div className="mb-[20px]">
-            <img src={photoTwo} alt="" className="w-full" />
+      <div className="grid grid-cols-3 grid-rows-2 gap-4">
+        <div className="relative col-span-3 row-span-2">
+          <img src={photoTwo} alt="" className="w-full" />
+          <div className="absolute bottom-[10%] left-[20px] hidden   w-[70%] text-white sm:block">
+            <h3 className="text-xl font-semibold md:text-3xl">
+              Women's Collections
+            </h3>
+            <p className="text-sm text-gray md:text-lg">
+              Featured Women's Collections That Give You Anther Vibe.
+            </p>
+            <Link className="text-xs  underline">Shop Now</Link>
           </div>
+        </div>
+        <div className="relative col-start-2 row-start-3">
+          <img src={photoOne} alt="" className="w-full" />
 
-          <div className="flex flex-col items-start gap-[20px] md:flex-row">
-            <div className="w-full md:w-[50%]">
-              <img src={photoThree} alt="" className="w-full" />
-            </div>
+          <div className="absolute bottom-[10%] left-[20px] hidden   w-[70%] text-white sm:block">
+            <h3 className="text-xl font-semibold md:text-3xl">
+              Play Station-5
+            </h3>
+            <p className="text-sm text-gray md:text-lg">
+              Black And White Version Of PS-5 Coming Out On Sale.
+            </p>
+            <Link className="text-xs  underline">Shop Now</Link>
+          </div>
+        </div>
+        <div className="relative col-start-1 row-start-3">
+          <img src={photoThree} alt="" className="w-full" />
 
-            <div className="w-full md:w-[50%]">
-              <img src={photoFour} alt="" className="w-full" />
-            </div>
+          <div className="absolute  bottom-[10%] left-[20px] hidden  w-[70%] text-white sm:block">
+            <h3 className="text-xl font-semibold md:text-3xl">Perfume</h3>
+            <p className="text-sm text-gray md:text-lg">
+              Cocci Intense Out EDP.
+            </p>
+            <Link className="text-xs  underline">Shop Now</Link>
+          </div>
+        </div>
+        <div className="relative row-start-3">
+          <img src={photoFour} alt="" className="w-full" />
+
+          <div className="absolute  bottom-[10%] left-[20px] hidden  w-[70%] text-white sm:block">
+            <h3 className="text-xl font-semibold md:text-3xl">Speakers</h3>
+            <p className="text-sm text-gray md:text-lg">
+              Amazon Wireless Speakers.
+            </p>
+            <Link className="text-xs  underline">Shop Now</Link>
           </div>
         </div>
       </div>
